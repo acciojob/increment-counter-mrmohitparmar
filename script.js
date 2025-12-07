c01b9400189ff0c5c87c6543d2043dbb3edd56cc
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-07 17:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// your JS code here. If required.
+
+let incrementBtn = document.getElementById("incrementBtn");
+
+function inc() {
+    let counterEl = document.getElementById("counter");
+    let counter = Number(counterEl.innerText); // Convert to number
+	
+    counter++;
+	alert(counter)
+    counterEl.innerText = counter; // Update UI
+}
+
+incrementBtn.addEventListener("click", inc);
